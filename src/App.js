b872f44b5557813611ecd8dee76dd1a70c7a8e5d
GuilleCalderon <guillermo.calde46@gmail.com>
2022-02-22 11:10:10 -0300

    inicio selects anidados

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CrudApi from "./components/CrudApi";
 import CrudApp from "./components/CrudApp";
+import SelectsAnidados from "./components/SelectsAnidados";
 import SongSearch from "./components/SongSearch";
 
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
-      
+      <hr />
+      <SelectsAnidados />
       <hr />
       <SongSearch />
       <hr />
